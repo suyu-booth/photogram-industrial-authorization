0350b1af16903420bb976f1883c397bbf86a2011
suyu-booth <suyu@chicagobooth.edu>
2025-04-06 21:46:22 +0000

    123

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
-  before_action :is_an_authorized_user, only: [:destroy, :create]
+  before_action :is_an_authorized_user, only: %i[ create]
+  before_action :is_an_authorized_editor, only: %i[destroy edit update]
 
   # GET /comments or /comments.json
   def index
@@ -72,6 +73,12 @@ class CommentsController < ApplicationController
     def is_an_authorized_user
       @photo = Photo.find(params.fetch(:comment).fetch(:photo_id))
       if current_user != @photo.owner && @photo.owner.private? && !current_user.leaders.include?(@photo.owner)
+        redirect_back fallback_location: root_url, alert: "Not authorized"
+      end
+    end
+
+    def is_an_authorized_editor
+      if current_user != @comment.author 
         redirect_back fallback_location: root_url, alert: "Not authorized"
       end
     end
